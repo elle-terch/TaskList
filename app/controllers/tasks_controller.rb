46c488d3a33703906d3023a36ae74304d1933872
Lindsay Terchin <lterchin@gmail.com>
2018-09-17 16:20:38 -0700

    added a parameterized route and a show method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,16 @@
 TASKS = ["walk dog", "brush teeth", "empty dishwasher", "pay student loans", "go for a run", "sleeeeeep"]
 
 
-
 class TasksController < ApplicationController
   def index
     @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    if @task.nil?
+      head :not_found
+    end
   end
 end
