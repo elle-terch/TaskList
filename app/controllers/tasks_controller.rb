2d5c0e1c340a7b9f70b6d2c07ea6e329c0e7b0a1
Lindsay Terchin <lterchin@gmail.com>
2018-09-20 14:56:24 -0700

    added a way for a user to enter form data, have that data get added to the database, and then redirect back to the main tasks page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,5 +17,19 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-  
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
+    )
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
 end
